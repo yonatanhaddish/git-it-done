85316584a1f3ef207092aaa7522e3b0cb06746af
Your Name <you@example.com>
2021-03-31 17:27:11 -0400

    error handler and linked two html pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,26 @@
 var issueContainerEl= document.querySelector("#issues-container");
+var limitWarningEl= document.querySelector("#limit-warning");
+var repoNameEl= document.querySelector("#repo-name");
 
-var getReopIssues= function(repo) {
+var getRepoName= function() {
+    
+    // grab repo name from url query string
+    var queryString= document.location.search;
+    var repoName= queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent= repoName;
+
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo name was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }   
+};
+
+var getRepoIssues= function(repo) {
 
     var apiUrl= "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     
@@ -9,10 +29,16 @@ var getReopIssues= function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                // check of api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not sucessful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -54,6 +80,20 @@ var displayIssues= function(issues) {
         // append to issue container
         issueContainerEl.appendChild(issueEl);
     }
+    console.log(issues.length);
 };
 
-getReopIssues("yonatanhaddish/refactor");
+var displayWarning= function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent= "To see more than 30 issues, visit ";
+
+    var linkEl= document.createElement("a");
+    linkEl.textContent= "See More Issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
